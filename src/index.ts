1a24cd62fd3471e8c28aa200bdaa6e9facdc195f
Alok Singh <salok2857@gmail.com>
2025-08-31 22:03:13 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import { UserModel } from "./db";
+const app = express();
+
+app.get("/api/v1/content", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  await UserModel.create({
+    username: username,
+    password: password,
+  });
+
+  res.json({
+    messaage: "Usser created Successfully",
+  });
+});
+
+app.delete("/api/v1/content", (req, res) => {});
+
+app.post("/api/v1/brain/share", (req, res) => {});
+
+app.get("/api/v1/breain/:shareLink", (req, res) => {});
