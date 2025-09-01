ebe51ce46ea08f8fa38b4605b2d27070be33ec2e
Alok Singh <salok2857@gmail.com>
2025-09-01 16:11:41 +0530

    fix all error

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,12 @@
 import mongoose, { model, Schema } from "mongoose";
 
-const MONGODB_URL = process.env.MONGODB_URL;
+// const MONGODB_URL = process.env.MONGODB_URL;
 
-if (!MONGODB_URL) {
-  throw new Error("MONGODB_URL is not set");
-}
+// if (!MONGODB_URL) {
+//   throw new Error("MONGODB_URL is not found");
+// }
 
-mongoose.connect(MONGODB_URL);
+mongoose.connect("mongodb+srv://alok:alok1234@cluster0.4ps2204.mongodb.net/");
 
 const UserSchema = new Schema({
   Username: { type: String, unique: true },
