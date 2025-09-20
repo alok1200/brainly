ce58e633c82658fdc1f650167bb3b170fcbcaa7a
Alok Singh <salok2857@gmail.com>
2025-09-20 16:07:14 +0530

    adding signin config and content config and addign jwt and usermiddleware

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
+import cors from "cors";
 import express from "express";
-// import jwt from "jsonwebtoken";
-import { UserModel } from "./db";
+import jwt from "jsonwebtoken";
+import { JWT_PASSWORD } from "./config";
+import { ContentMobel, UserModel } from "./db";
+import { userMiddleware } from "./middleware";
+
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.post("/api/v1/signup", async (req, res) => {
   // TODO: zod validation , hash the password
@@ -24,7 +30,49 @@ app.post("/api/v1/signup", async (req, res) => {
   }
 });
 
-// app.delete("/api/v1/content", (req, res) => {});
+app.delete("/api/v1/sighin", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const existingUser = UserModel.findOne({
+    username,
+    password,
+  });
+  if (existingUser) {
+    const token = jwt.sign(
+      {
+        id: existingUser._id,
+      },
+      JWT_PASSWORD
+    );
+
+    res.json({
+      token,
+    });
+  } else {
+    res.status(403).json({
+      message: "Incorrrect credentials",
+    });
+  }
+});
+
+app.post("/api/v1/brain/content", userMiddleware, async (req, res) => {
+  const link = req.body.link;
+  const type = req.body.type;
+  await ContentMobel.create({
+    link,
+    type,
+    userId: req.userId,
+    title: req.body.title,
+    tags: [],
+  });
+
+  res.json({
+    message: "Content added",
+  });
+});
+
+app.post("/api/v1/brain/content", userMiddleware, async (req, res) => {});
 
 // app.post("/api/v1/brain/share", (req, res) => {});
 
